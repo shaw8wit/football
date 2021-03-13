c6a7ea12c1bc15fec3ce6e3ae8900949645500b0
shaw8wit <shaw8wit@gmail.com>
2021-03-13 23:24:51 +0530

    initializes country and search components

---
+++ src/app/search.service.ts
@@ -0,0 +1,14 @@
+import { HttpClientModule } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SearchService {
+
+  constructor(private http: HttpClientModule) { }
+
+  fetchCountries() {
+
+  }
+}
